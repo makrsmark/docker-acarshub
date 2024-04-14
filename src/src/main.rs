f8f937614cc322cf6a8ba8980840f57699d38d57
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2024-04-14 02:07:02 -0600

    rust base init

---
+++ src/src/main.rs
@@ -0,0 +1,7 @@
+// Entry file for ACARS Hub
+
+#![deny(clippy::pedantic, clippy::cargo, clippy::nursery, clippy::style, clippy::correctness, clippy::all)]
+
+fn main() {
+    println!("Hello, world!");
+}
